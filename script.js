3167b3be5ef22a27dd3934665f5137d7f0ce4125
Hunter Haller <huntinghollers@proton.me>
2025-03-19 15:42:22 +0100

    Established working operation functions.

---
+++ script.js
@@ -0,0 +1,25 @@
+let numberA = 0;
+let numberB = 0;
+let result = 0;
+
+function add(numA, numB){
+    return numA + numB;
+}
+
+function subtract(numA, numB){
+    return numA - numB;
+}
+
+function multiply(numA, numB){
+    return numA * numB;
+}
+
+function divide(numA, numB){
+    return numA / numB;
+}
+
+//Operation tests
+console.log(add(2, 2))
+console.log(subtract(2, 2))
+console.log(multiply(2, 2))
+console.log(divide(2, 2))
