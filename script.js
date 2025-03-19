17f441e92f77d7e0d3ad861ff725056e037666b6
Hunter Haller <huntinghollers@proton.me>
2025-03-19 15:48:47 +0100

    Established working "operate" function.

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 let numberA = 0;
 let numberB = 0;
 let result = 0;
+let operator = "";
 
 function add(numA, numB){
     return numA + numB;
@@ -18,8 +19,27 @@ function divide(numA, numB){
     return numA / numB;
 }
 
-//Operation tests
-console.log(add(2, 2))
-console.log(subtract(2, 2))
-console.log(multiply(2, 2))
-console.log(divide(2, 2))
+function operate(numA, operation, numB){
+    switch(operation){
+        case "+":
+            return add(numA, numB);
+            break;
+        case "-":
+            return subtract(numA, numB);
+            break;
+        case "*":
+            return multiply(numA, numB);
+            break;
+        case "/":
+            return divide(numA, numB);
+            break;
+        default:
+            return "ERROR"
+            break;
+    }
+}
+
+console.log(operate(2, "+", 2))
+console.log(operate(2, "-", 2))
+console.log(operate(2, "*", 2))
+console.log(operate(2, "/", 2))
